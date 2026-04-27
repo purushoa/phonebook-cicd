9d09632a1d5eb7e89019c957e2c82b5db4fc7017
purushoa <purushottamadh1121@gmail.com>
2026-04-28 02:27:45 +0300

    chore: final fix for tests and pipeline

--- e2e-tests/pokedex.spec.js
+++ e2e-tests/pokedex.spec.js
@@ -4,7 +4,7 @@ const { test, expect, describe } = require('@playwright/test');
 describe('Pokedex', () => {
   test('front page can be opened', async ({ page }) => {
     // We point to localhost where the CI will run the app
-    await page.goto('http://localhost:8080');
+    await page.goto('http://127.0.0.1:8080');
     await expect(page.getByText('ivysaur')).toBeVisible();
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible();
   });
